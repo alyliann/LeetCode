f3250d5f1507b07e8f35f453140d9a2a8acb3ca7
Alysa Vega <92405765+alyliann@users.noreply.github.com>
2022-09-29 22:01:34 -0400

    Create 1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,26 @@
+//Author: Alysa Vega
+//Submission: https://leetcode.com/submissions/detail/794772385/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        
+        vector<int> twoSum;
+        
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = 0; j < nums.size(); j++)
+            {
+                if (nums[i] + nums[j] == target && i != j)
+                {
+                    twoSum.push_back(i);
+                    twoSum.push_back(j);
+                    return twoSum;
+                }
+            }
+        }
+        
+        return twoSum;
+        
+    }
+};
