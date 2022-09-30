18cf378eecd8a1f3a1124f50d8135dfd0e909ce5
Alysa Vega <92405765+alyliann@users.noreply.github.com>
2022-09-29 22:02:58 -0400

    Create 9. Palindrome Number.cpp

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,22 @@
+//Author: Alysa Vega
+//Submission: https://leetcode.com/submissions/detail/795080310/
+
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        
+        if (x < 0)
+            return false;
+        
+        string num = to_string(x);
+        
+        for (int i = 0; i < num.size()/2; i++)
+        {
+            if (num[i] != num[num.size()-i-1])
+                return false;
+        }
+        
+        return true;
+        
+    }
+};
